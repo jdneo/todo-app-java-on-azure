9d289f515216ca86db1d9b309589c4654674682c
Sheng Chen <sheche@microsoft.com>
2018-04-09 16:07:30 +0800

    hands-on lab build

--- src/test/java/com/microsoft/azure/sample/model/TodoItemTest.java
+++ src/test/java/com/microsoft/azure/sample/model/TodoItemTest.java
@@ -13,16 +13,26 @@ public class TodoItemTest {
 
     @Test
     public void testEqualsObject() {
-        final TodoItem itemA = new TodoItem();
-        final TodoItem itemB1 = new TodoItem("B", "Item B", "Owner of Item B");
-        final TodoItem itemB2 = new TodoItem("B", "Item B", "Owner of Item B");
-        final Object nonTodoItem = new Object();
-        assertTrue(itemA.equals(itemA));
-        assertFalse(itemA.equals(null));
-        assertFalse(itemA.equals(nonTodoItem));
+        final TodoItem itemA = new TodoItem("A", "Item A", "Owner of Item A");
+        final TodoItem itemB1 = new TodoItem("B", "Item B", "Owner of Item B");   
+        
         assertFalse(itemA.equals(itemB1));
-        assertTrue(itemB1.equals(itemB2));
-        assertFalse(itemB1.equals(itemA));
+
+        /**
+         * Let user define a new TodoItem itemB2, whose constructor is the same as itemB1.
+         * And verify if itemB1 is equal to itemB2.
+         * 
+         * Answer:
+         * final TodoItem itemB2 = new TodoItem("B", "Item B", "Owner of Item B");
+         * assertTrue(itemB1.equals(itemB2));
+         * 
+         * ----------------------------------------
+         * Here are some other options users can do:
+         * 
+         * 1. Add whatever test cases they want to add and have a try.
+         * 2. Set a breakpoint in method: equals() of TodoItem.java, and experience the debug test.
+         *  
+         */
     }
 
 }
